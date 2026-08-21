eb6f0ed31f2b7126d05cec3a78fca47ec9b28c6e
rpritika066-max <238410415+rpritika066-max@users.noreply.github.com>
2026-08-21 11:32:28 +0530

    perf: profile slow author quotes endpoint

--- day-2/QuotesApi/Models/Quote.cs
+++ day-2/QuotesApi/Models/Quote.cs
@@ -3,6 +3,12 @@ namespace QuotesApi.Models;
 public class Quote
 {
     public int Id { get; set; }
+
     public string Author { get; set; } = string.Empty;
+
+    public int? AuthorId { get; set; }
+
+    public Author? AuthorEntity { get; set; }
+
     public string Text { get; set; } = string.Empty;
 }
